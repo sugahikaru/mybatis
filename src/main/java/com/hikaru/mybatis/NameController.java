88e30a5f240b4f7d81fe320fa60ac7047523de26
sugahikaru <sh09054713265@outlook.jp>
2024-05-03 13:46:57 +0900

    Read処理の実装

---
+++ src/main/java/com/hikaru/mybatis/NameController.java
@@ -0,0 +1,19 @@
+package com.hikaru.mybatis;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class NameController {
+    private final NameMapper nameMapper;
+    public NameController(NameMapper nameMapper) {
+        this.nameMapper = nameMapper;
+    }
+    @GetMapping("/names")
+    public List<Name> findByNames(@RequestParam String startsWith) {
+        return nameMapper.findByNameStartingWith(startsWith);
+    }
+}
---
+++ src/main/java/com/hikaru/mybatis/NameMapper.java
@@ -0,0 +1,18 @@
+package com.hikaru.mybatis;
+
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface NameMapper {
+
+    @Select("SELECT * FROM names")
+    List<Name> findAll();
+
+    @Select("SELECT * FROM names WHERE name LIKE CONCAT(#{prefix}, '%')")
+    List<Name> findByNameStartingWith(String prefix);
+
+}
