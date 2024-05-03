c5812ef723eb89caa5d44b3847aea75a11a85810
sugahikaru <163830579+sugahikaru@users.noreply.github.com>
2024-05-03 13:51:07 +0900

    Update NameMapper.java

--- src/main/java/com/hikaru/mybatis/NameMapper.java
+++ src/main/java/com/hikaru/mybatis/NameMapper.java
@@ -9,9 +9,6 @@ import java.util.List;
 @Mapper
 public interface NameMapper {
 
-    @Select("SELECT * FROM names")
-    List<Name> findAll();
-
     @Select("SELECT * FROM names WHERE name LIKE CONCAT(#{prefix}, '%')")
     List<Name> findByNameStartingWith(String prefix);
 
